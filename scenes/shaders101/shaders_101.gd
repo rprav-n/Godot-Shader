d948511cf0b89d1eca8d1a3735d613af0609005b
Praveen <rpraveen.engineer@gmail.com>
2024-05-04 14:46:59 +0530

    sin and cos in shaders

---
+++ scenes/shaders101/shaders_101.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+#
+#func _process(delta: float) -> void:
+	#var TIME = Time.get_ticks_msec() / 1000.0
+	#var a = (sin(TIME) + 1.0) * 0.5;
+	##print("Time: " + str(TIME))
+	#print("a: ", a)
